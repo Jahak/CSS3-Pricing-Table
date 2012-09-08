4d3e24ecf76a770ae49133a4f706337d2a9e2f46
Paul Underwood <paul.und@gmail.com>
2012-09-08 14:10:39 +0100

    CSS3 Pricing Table

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+$title = "CSS3 Pricing Table - Demo";
+$blog_post = "http://www.paulund.co.uk/css3-animation-pricing-table";
+
+require_once '../header.php';
+?>
+
+<h2><?php echo $title; ?></h2>
+<p>Below is a demo of creating a pricing table in CSS3. It consists of a whole bunch of new CSS3 features including : transforms, gradient, shadow and nth-child.</p>
+
+<p>Hover your mouse over the price table to see the CSS animation in action.</p>
+
+<div class="pricing_table">
+		<ul>
+			<li>Standard</li>
+			<li>&pound;20</li>
+			<li>Perfect for those getting started with our app.</li>
+			<li>15 Projects</li>
+			<li>5GB Storage</li>
+			<li>Unlimited Users</li>
+			<li>No Time Tracking</li>
+			<li>Enchaned Security</li>
+			<li></li>
+			<li><a href="" class="buy_now">Buy Now</a></li>
+		</ul>
+		
+		<ul>
+			<li>Premium</li>
+			<li>&pound;60</li>
+			<li>Perfect for those getting started with our app.</li>
+			<li>15 Projects</li>
+			<li>5GB Storage</li>
+			<li>Unlimited Users</li>
+			<li>No Time Tracking</li>
+			<li>Enchaned Security</li>
+			<li></li>
+			<li><a href="" class="buy_now">Buy Now</a></li>
+		</ul>
+		
+		<ul>
+			<li>Professional</li>
+			<li>&pound;80</li>
+			<li>Perfect for those getting started with our app.</li>
+			<li>15 Projects</li>
+			<li>5GB Storage</li>
+			<li>Unlimited Users</li>
+			<li>No Time Tracking</li>
+			<li>Enchaned Security</li>
+			<li></li>
+			<li><a href="" class="buy_now">Buy Now</a></li>
+		</ul>
+		
+		<ul>
+			<li>Plus</li>
+			<li>&pound;100</li>
+			<li>Perfect for those getting started with our app.</li>
+			<li>15 Projects</li>
+			<li>5GB Storage</li>
+			<li>Unlimited Users</li>
+			<li>No Time Tracking</li>
+			<li>Enchaned Security</li>
+			<li></li>
+			<li><a href="" class="buy_now">Buy Now</a></li>
+		</ul>
+		
+		<ul>
+			<li>Maximum</li>
+			<li>&pound;120</li>
+			<li>Perfect for those getting started with our app.</li>
+			<li>15 Projects</li>
+			<li>5GB Storage</li>
+			<li>Unlimited Users</li>
+			<li>No Time Tracking</li>
+			<li>Enchaned Security</li>
+			<li></li>
+			<li><a href="" class="buy_now">Buy Now</a></li>
+		</ul>
+</div>
+
+<br style="clear:both;" />
+
+<section class="aligncenter">
+	<p><a href="#" class="download_button">Tweet To Download Tutorial</a></p>
+</section>
+	
+
+<?php require_once '../footer.php'; ?>
